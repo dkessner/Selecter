e0a142b700cef3ea16b7e7224ebf6fe6b49066a9
dkessner laptop <Darren.Kessner@marlborough.org>
2022-07-01 14:37:02 -0700

    added Selecter class

---
+++ Selecter.js
@@ -0,0 +1,82 @@
+//
+// Selecter.js
+//
+
+
+class Selecter
+{
+    constructor()
+    {
+        this.targets = [];
+        this.selectedTarget = null;
+    }
+
+    displayTargets()
+    {
+        for (let t of this.targets)
+            t.display(t === this.selectedTarget);
+    }
+
+    selectBestTarget()
+    {
+        let minDistance = width;
+        let minTarget = null;
+
+        for (let t of this.targets)
+        {
+            let d = dist(t.position.x, t.position.y, mouseX, mouseY);
+            if (d < t.radius && d < minDistance)
+            {
+                minDistance = d;
+                minTarget = t;
+            }
+        }
+
+        this.selectedTarget = minTarget;
+    }
+
+    dragSelectedTarget()
+    {
+        if (this.selectedTarget)
+        {
+            this.selectedTarget.position.x = mouseX;
+            this.selectedTarget.position.y = mouseY;
+            this.selectedTarget.constrain();
+        }
+    }
+}
+
+
+class Target
+{
+    constructor(x, y, radius = 25) 
+    {
+        this.position = createVector(x, y);
+        this.radius = radius;
+        this.colorFill = color(128);
+        this.colorStroke = this.colorFill;
+        this.colorFillSelected = this.colorFill;
+        this.colorStrokeSelected = color(0, 255, 0);
+    }
+
+    display(isSelected) 
+    {
+        stroke(isSelected ? this.colorStrokeSelected : this.colorStroke);
+        strokeWeight(isSelected ? 3 : 0);
+        fill(isSelected ? this.colorFillSelected : this.colorFill);
+
+        ellipse(this.position.x, this.position.y, this.radius, this.radius);
+    }
+
+    constrain(){}
+
+    constrainCircle(center, radius)
+    {
+        this.position.sub(center);
+        this.position.setMag(radius);
+        this.position.add(center);
+    }
+}
+
+
+
--- pick.js
+++ pick.js
@@ -3,33 +3,31 @@
 //
 
 
-let targets;
-let selectedTarget;
-
+let selecter;
 let segment;
 
 
 function setup()
 {
     createCanvas(400, 400);
-    targets = [];
-    selectedTarget = null;
+
+    selecter = new Selecter();
 
     for (let i=0; i<5; i++)
-        targets.push(new Target(random(0, width), random(0, height)));
+        selecter.targets.push(new Target(random(0, width), random(0, height)));
 
     for (let i=0; i<5; i++)
     {   
         let t = new Target(random(0, width), random(0, height));
-        t.c = color(0, 0, 128);
+        t.colorFill = t.colorFillSelected = color(0, 0, 128);
         t.constrain = () => t.constrainCircle(createVector(200, 200), 100);
         t.constrain();
-        targets.push(t);
+        selecter.targets.push(t);
     }
 
-    segment = new Segment(targets[0], targets[5]);
-    targets[0].c = color(255, 0, 0);
-    targets[5].c = color(0, 0, 255);
+    segment = new Segment(selecter.targets[0], selecter.targets[5]);
+    selecter.targets[0].colorFill = color(255, 0, 0);
+    selecter.targets[5].colorFill = color(0, 0, 255);
 }
 
 
@@ -37,13 +35,13 @@ function draw()
 {
     background(0);
 
+    // constraint circle
     stroke(128);
-    strokeWeight(1);
+    strokeWeight(2);
     noFill();
     ellipse(200, 200, 200, 200);
 
-    for (let t of targets)
-        t.display(t === selectedTarget);
+    selecter.displayTargets();
 
     segment.display();
 }
@@ -51,27 +49,9 @@ function draw()
 
 function mouseMoved()
 {
-    selectedTarget = selectBestTarget();
+    selecter.selectBestTarget();
 }
 
-
-function selectBestTarget()
-{
-    let minDistance = width;
-    let minTarget = null;
-
-    for (let t of targets)
-    {
-        let d = dist(t.position.x, t.position.y, mouseX, mouseY);
-        if (d < t.r && d < minDistance)
-        {
-            minDistance = d;
-            minTarget = t;
-        }
-    }
-
-    return minTarget;
-}
 
 
 // function mousePressed() {} 
@@ -79,51 +59,11 @@ function selectBestTarget()
 
 function mouseDragged()
 {
-    if (selectedTarget)
-    {
-        selectedTarget.position.x = mouseX;
-        selectedTarget.position.y = mouseY;
-        selectedTarget.constrain();
-    }
+    selecter.dragSelectedTarget();
 }
 
 // function mouseReleased() {}
 
-
-class Target
-{
-    constructor(x, y) 
-    {
-        this.position = createVector(x, y);
-        this.r = 25;
-        this.c = color(255);
-    }
-
-    display(selected) 
-    {
-        if (selected)
-        {
-            stroke(0, 255, 0);
-            strokeWeight(5);
-        }
-        else
-        {
-            noStroke();
-        }
-
-        fill(this.c);
-        ellipse(this.position.x, this.position.y, this.r, this.r);
-    }
-
-    constrain(){}
-
-    constrainCircle(center, radius)
-    {
-        this.position.sub(center);
-        this.position.setMag(radius);
-        this.position.add(center);
-    }
-}
 
 
 class Segment
